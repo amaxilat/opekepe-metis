bc243aa28c0ae8ce94258e013d0fee63186254f6
amaxilat <d.amaxilatis@gmail.com>
2021-12-20 09:46:41 +0200

    update conditions

--- util/src/main/java/com/amaxilatis/metis/config/Conditions.java
+++ util/src/main/java/com/amaxilatis/metis/config/Conditions.java
@@ -2,6 +2,6 @@ package com.amaxilatis.metis.config;
 
 public class Conditions {
     public static final double N1_PIXEL_SIZE = 0.5;
-    public static final Integer N2_BIT_SIZE = 11;
+    public static final Integer N2_BIT_SIZE = 8;
     public static final int N3_SAMPLES_PER_PIXEL = 4;
 }
