990b8dba16e37231ece3588f93b1e9db4d688db5
amaxilat <d.amaxilatis@gmail.com>
2021-12-21 11:56:19 +0200

    add dependency check

---
+++ server/src/main/java/com/amaxilatis/metis/server/model/ReportFileInfo.java
@@ -0,0 +1,19 @@
+package com.amaxilatis.metis.server.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ReportFileInfo implements Comparable<ReportFileInfo> {
+    private final String name;
+    private final String date;
+    private final String directory;
+    private final String path;
+    private final double size;
+    
+    @Override
+    public int compareTo(ReportFileInfo o) {
+        return name.compareTo(o.getName());
+    }
+}
